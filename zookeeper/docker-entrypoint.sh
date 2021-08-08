f6e3cb86e1fe546a647aaf2b9cc506250be00bbf
akinboj <akinboj@hotmail.com>
2021-08-09 09:44:08 +1000

    Updated Dockerfile and entrypoint.sh

--- zookeeper/docker-entrypoint.sh
+++ zookeeper/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Allow the container to be started with `--user`
-if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
+if [[ "$1" = '/apache-zookeeper-3.6.3-bin/bin/zkServer.sh' && "$(id -u)" = '0' ]]; then
     chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR"
     exec gosu zookeeper "$0" "$@"
 fi
